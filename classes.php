9eb6db72233671c8aa4b249fefe05f629afc7544
Davide <musitelli.davideluigi.studente@itispaleocapa.it>
2022-08-18 14:02:09 +0200

    Updating classes file

--- classes.php
+++ classes.php
@@ -1,29 +1,30 @@
 <?php  
-    class user{
-        public $name;
-        public $surname;
-        public $age;
-        //Constructor
-        function __construct($name, $surname){
-            $this->name = $name;
-            $this->surname = $surname;
-        }
-        //Get and set
-        function set_age($age){
-            $this->age = $age;
-        }
-        function get_age(){
-            return $this->age;
-        }
-        //Methods
-        function methodExample(){
-            echo "Hi, I am $this->name $this->surname";   
-        }
+class user{
+    public $name;
+    public $surname;
+    public $age;
+    //Constructor
+    function __construct($name, $surname){
+        $this->name = $name;
+        $this->surname = $surname;
     }
+    //Get and set
+    function set_age($age){
+        $this->age = $age;
+    }
+    function get_age(){
+        return $this->age;
+    }
+    //Methods
+    function userExample(){
+        echo "Hi, I am $this->name $this->surname";   
+    }
+}
     
-    $firstUser = new user("Mario" , "Rossi");
-    $firstUser->set_age(12);
-    echo $firstUser->get_age();
+$firstUser = new user("Mario" , "Rossi");
+$firstUser->set_age(12);
+//echo $firstUser->get_age();
+$firstUser->userExample();
 
     class teacher extends user{
         public $subject;
@@ -33,4 +34,15 @@
             $this->surname = $surname;
             $this->subject = $subject;
         }
+        
+        function teacherExample(){
+            echo "Hi, I am $this->name $this->surname, and I teach $this->subject";   
+        }
+
+        function example(){
+            $this->userExample();
+        }
     }
+
+$firstTeacher = new teacher("Mario","Bianchi","Matematica");
+$firstTeacher->teacherExample();
